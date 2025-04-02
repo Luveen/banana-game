2cf9c4e460485de7b19dd192364fbc1656e24db3
Luveen <mishanluveen08@gmail.com>
2025-04-02 20:57:29 +0530

    changed the border radius of balloon container

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css';
 import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  
+createRoot(document.getElementById('root')).render( 
     <App />
-    
-
 );
